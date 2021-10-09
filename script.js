37ce1af69653e8a5504a034b3c91bfb3ee086ed6
Mohamed Maaiz <mohammadh3g@gmail.com>
2021-10-09 15:11:21 +0500

    changed the function and added restart option

--- script.js
+++ script.js
@@ -3,43 +3,65 @@
 const Players = (name, mark) => {
     const getName = () => name
     const getMark = () => mark
-    let _playerChoices = []
+    let _playerChoices = []    
 
+    const pushChoice = (choice) => {
+        _playerChoices.push(parseInt(choice))
+        checkWin(parseInt(choice), _playerChoices, name)
+    }
+
+    const clear = () => {
+        _playerChoices.length = 0
+    }
+
+    return {getName, getMark, pushChoice, clear,_playerChoices}
+};
+
+const checkWin = (choice, playerChoices, name) => {
+    
     const _winingChoice = [
         [0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6],
         [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6],
     ];
+    
+    let _filteredList = _winingChoice.filter(choices => choices.includes(choice))
 
-    const _check = (choice) => {
-        let _filteredList = _winingChoice.filter(choices => choices.includes(choice))
-
-        for (let i = 0; i < _filteredList.length; i++) {
-            const result = _filteredList[i].every(val => _playerChoices.includes(val));
-
-            if (result == true) {
-                console.log(getName())
-            }
-            // console.log(result);
-        } 
+    const _winner = (name) => {
+        const winner = document.getElementById('winner')
+        winner.textContent = name
     }
 
-    const pushChoice = (choice) => {
-        _playerChoices.push(parseInt(choice))
-        
-        // console.log(player1._playerChoices)
-        // console.log(player2._playerChoices)
+    for (let i = 0; i < _filteredList.length; i++) {
+        const result = _filteredList[i].every(val => playerChoices.includes(val));
 
-        _check(parseInt(choice))
-    }
+        if (result == true) {
+            return _winner(name)
+        }
+        // console.log(result);
+    } 
+}
 
-    return {getName, getMark, pushChoice,_playerChoices}
-};
 
-const player1 = Players('him', 'X')
-const player2 = Players('them', 'O')
+const playerAssign = () =>{
+    const p1 = document.getElementById('p1').value
+    const p2 = document.getElementById('p2').value
+    const start = document.getElementById('start')
+    
+    const p1display = document.getElementById('firstP')
+    const p2display = document.getElementById('secondP')
+    p1display.textContent = p1
+    p2display.textContent = p2
+    
+    // start.onclick = () => Gameboard(player1,player2)
+    return{p1, p2}
+}
 
-const Gameboard = () => {
-    let bord = ['','','','','','', '','','',]
+
+const Gameboard = (() => {
+    const player1 = Players(playerAssign().p1, 'X')
+    const player2 = Players(playerAssign().p2, 'O')
+
+    // let bord = ['','','','','','', '','','',]
 
     const _board = document.querySelectorAll('.board')
 
@@ -53,15 +75,26 @@ const Gameboard = () => {
         nowPlay == player2 ? nowPlay = player1 : nowPlay = player2
     }
 
+    const Rstart = document.getElementById('re-start')
+    Rstart.onclick = () => clearAll()
+    
+    const clearAll = () => {
+        player1.clear()
+        player2.clear()
+        _board.forEach(box => box.textContent = '')
+    }
+
     const addMark = (target) => {
         let boxID = target.getAttribute('data-index')
-        bord[boxID] = nowPlay.getMark()
+        // bord[boxID] = nowPlay.getMark()
         target.textContent = nowPlay.getMark()
         nowPlay.pushChoice(boxID)
         _currentPlayer()
+
+        // console.log(player1._playerChoices)
+        // console.log(player2._playerChoices)
     }
     
     return {addMark}
-}
+})()
 
-Gameboard().addMark
